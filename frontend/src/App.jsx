2d4f071457fcfe63c68d1986a6b9d59507ff60e6
ShonKalangutkar <shonkalangutkar1998@gmail.com>
2026-02-24 10:45:34 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+import Login from "./assets/pages/login"
+import Register from "./assets/pages/Register"
+import Home from "./assets/pages/Home"
+import NotFound from "./assets/pages/NotFound"
+import ProtectedRoute from "./assets/components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
